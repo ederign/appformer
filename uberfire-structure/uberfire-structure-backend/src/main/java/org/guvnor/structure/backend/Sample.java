d93fb87dba209558727da65255d44140c7f63e95
ederign <ignatowicz@gmail.com>
2018-10-02 10:53:49 -0400

    maxd sample

---
+++ uberfire-structure/uberfire-structure-backend/src/main/java/org/guvnor/structure/backend/Sample.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2015 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+package org.guvnor.structure.backend;
+
+import org.guvnor.structure.config.AddPomDependency;
+import org.guvnor.structure.server.config.ConfigGroup;
+import org.guvnor.structure.server.config.ConfigItem;
+import org.guvnor.structure.server.config.ConfigurationFactory;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
+import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.List;
+
+@ApplicationScoped
+public class Sample {
+
+
+    public Sample() {
+    }
+
+    public void addDependnecy(@Observes AddPomDependency addPomDependency){
+        System.out.println("ADD");
+    }
+
+}
